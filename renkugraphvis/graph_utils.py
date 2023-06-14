e8d1c15a588d3bb94c92b2e978085d8f8b19cfb3
burnout87 <gabriele.barni@gmail.com>
2023-06-14 14:34:37 +0200

    removed inspect cli and annotation logging

--- renkugraphvis/graph_utils.py
+++ renkugraphvis/graph_utils.py
@@ -3,33 +3,25 @@ import io
 import typing
 import pydotplus
 import rdflib
-import yaml
 import json
-import hashlib
 import glob
 import urllib.request
 import pathlib
 
-from prettytable import PrettyTable
 from rdflib.tools.rdf2dot import LABEL_PROPERTIES
 from rdflib.tools import rdf2dot
 from lxml import etree
 from dateutil import parser
 from astropy.coordinates import SkyCoord, Angle
-from IPython.display import display
 from pyvis.network import Network
-from importlib import resources
-from nb2workflow import ontology
 
 from renku.domain_model.project_context import project_context
 from renku.command.graph import export_graph_command
 from renku.core.errors import RenkuException
-from renku.core.util.git import get_entity_from_revision
 
 import renkugraphvis.javascript_graph_utils as javascript_graph_utils
 
 from renkugraphvis.config import ENTITY_METADATA_GRAPHVIS_DIR
-from renkugraphvis.plugin import GraphVis
 
 # TODO improve this
 __this_dir__ = os.path.join(os.path.abspath(os.path.dirname(__file__)))
@@ -223,97 +215,6 @@ def build_graph_html(revision, paths,
 
     # return net, html_fn
     return net.html, graph_str
-
-
-def inspect_entity_inputs(revision, paths, input_notebook: str = None):
-    if paths is None:
-        paths = project_context.path
-
-    graph = _renku_graph(revision, paths)
-
-    query_select = "SELECT DISTINCT ?entityInput ?entityInputLocation ?entityInputChecksum"
-
-    query_where = """WHERE {
-                    ?entityInput a <http://www.w3.org/ns/prov#Entity> ;
-                        <http://www.w3.org/ns/prov#atLocation> ?entityInputLocation ;
-                        <https://swissdatasciencecenter.github.io/renku-ontology#checksum> ?entityInputChecksum .
-            """
-
-    if input_notebook is not None:
-        query_where += f"""
-        
-                FILTER ( ?entityInputLocation = '{input_notebook}' ) .
-        """
-
-    query_where += """
-            ?activity a ?activityType ;
-                <http://www.w3.org/ns/prov#qualifiedUsage>/<http://www.w3.org/ns/prov#entity> ?entityInput .        
-    }
-    """
-
-    query = f"""{query_select}
-               {query_where}
-            """
-
-    r = graph.query(query)
-
-    G = rdflib.Graph()
-
-    output = PrettyTable()
-    output.field_names = ["Entity ID", "Entity checksum", "Entity input location"]
-    output.align["Entity ID"] = "l"
-    output.align["Entity checksum"] = "l"
-    output.align["Entity input location"] = "l"
-    for row in r:
-        entity_path = row.entityInputLocation
-        entity_checksum = row.entityInputChecksum
-        entity_id = row.entityInput
-        output.add_row([
-            entity_id,
-            entity_checksum,
-            entity_path
-        ])
-        entity_file_name, entity_file_extension = os.path.splitext(entity_path)
-        if entity_file_extension == '.ipynb':
-            print(f"\033[31mExtracting metadata from the input notebook: {entity_path}, id: {entity_id}\033[0m")
-            # get checksum from the path
-            repository = project_context.repository
-            revision = repository.head.commit.hexsha
-            entity_obj = get_entity_from_revision(repository=repository, path=entity_path, revision=revision, bypass_cache=True)
-            print(f"\033[31mEntity object extracted from entity_path: {entity_obj.path}, checksum: {entity_obj.checksum}\033[0m")
-            print(f"\033[31mEntity checksum from the graph: {entity_checksum}\033[0m")
-            if str(entity_obj.checksum) == str(entity_checksum):
-                # file present on disk based on the checksum equality
-                rdf_nb = ontology.nb2rdf(entity_path)
-                graphvis_obj = GraphVis(entity_path)
-                G.parse(data=rdf_nb)
-                rdf_jsonld_str = G.serialize(format="json-ld")
-                rdf_jsonld = json.loads(rdf_jsonld_str)
-
-                print(f"\033[32mlog_aqs_annotation\033[0m")
-
-                annotation_folder_path = pathlib.Path(
-                    os.path.join(graphvis_obj.graph_vis_annotation_path, entity_file_name, entity_checksum))
-                if annotation_folder_path.exists():
-                    # directory gets cleaned-up in order to avoid to generate duplicate jsonld files
-                    # that can occur in case of new commits where input notebook is not affected
-                    jsonld_files = glob.glob(str(annotation_folder_path.joinpath("*.jsonld")))
-                    for j_f in jsonld_files:
-                        os.remove(j_f)
-                else:
-                    annotation_folder_path.mkdir(parents=True)
-
-                for nb2annotation in rdf_jsonld:
-                    nb2annotation["http://odahub.io/ontology#entity_checksum"] = entity_checksum
-                    print(f"found jsonLD annotation:\n", json.dumps(nb2annotation, sort_keys=True, indent=4))
-                    nb2annotation_id_hash = hashlib.sha256(nb2annotation["@id"].encode()).hexdigest()[:8]
-
-                    jsonld_path = os.path.join(annotation_folder_path, nb2annotation_id_hash + ".jsonld")
-                    with open(jsonld_path, mode="w") as f:
-                        print("writing", jsonld_path)
-                        f.write(json.dumps(nb2annotation, sort_keys=True, indent=4))
-
-    print(output, "\n")
 
 
 def build_graph_image(revision, paths, filename, input_notebook):
--- renkugraphvis/plugin.py
+++ renkugraphvis/plugin.py
@@ -23,126 +23,13 @@ import re
 import subprocess
 import webbrowser
 import click
-import rdflib
-import rdflib.tools.rdf2dot
 
-from renku.domain_model.provenance.annotation import Annotation
 from renku.domain_model.project_context import project_context
-from renku.core.plugin import hookimpl
 from IPython.display import Image, HTML
 from prettytable import PrettyTable
 from renkugraphvis.config import ENTITY_METADATA_GRAPHVIS_DIR
-from nb2workflow import ontology
 
 import renkugraphvis.graph_utils as graph_utils
-
-
-class GraphVis(object):
-    def __init__(self, run):
-        self.run = run
-
-    @property
-    def graph_vis_annotation_path(self):
-        """Return a ``Path`` instance of Renku GraphVis specific annotation."""
-        return pathlib.Path(project_context.metadata_path).joinpath(ENTITY_METADATA_GRAPHVIS_DIR)
-
-    def load_model(self, path):
-        """Load GraphVis reference file."""
-        if path and path.exists():
-            return json.load(path.open())
-        return {}
-
-
-@hookimpl
-def activity_annotations(activity):
-    """``process_run_annotations`` hook implementation."""
-    graphvis = GraphVis(activity)
-
-    sitecustomize_path = pathlib.Path(os.path.join(project_context.metadata_path, ENTITY_METADATA_GRAPHVIS_DIR, "sitecustomize.py"))
-    if sitecustomize_path.exists():
-        sitecustomize_path.unlink()
-
-    annotations = []
-
-    print("process_run_annotations")
-    print(graphvis.graph_vis_annotation_path)
-    # apply nb2rdf also to input nb and also add the name of the notebook
-    # should be related to the input/output notebook
-    # add the annotations to the plan
-
-    if activity.generations is not None and len(activity.generations) >= 1:
-        for generation in activity.generations:
-            entity = generation.entity
-            if isinstance(entity, list):
-                entity = generation.entity[0]
-            entity_file_name, entity_file_extension = os.path.splitext(entity.path)
-            if entity_file_extension == '.ipynb':
-                print(f"\033[31mExtracting metadata from the output notebook: {entity.path}, id: {entity.id}\033[0m")
-                rdf_nb = ontology.nb2rdf(entity.path)
-                print(f"\033[32m{rdf_nb}\033[0m")
-                G = rdflib.Graph()
-                G.parse(data=rdf_nb)
-                rdf_jsonld_str = G.serialize(format="json-ld")
-                rdf_jsonld = json.loads(rdf_jsonld_str)
-                for nb2annotation in rdf_jsonld:
-                    # to comply with the terminology
-                    nb2annotation["http://odahub.io/ontology#entity_checksum"] = entity.checksum
-                    print(f"found jsonLD annotation:\n", json.dumps(nb2annotation, sort_keys=True, indent=4))
-                    model_id = nb2annotation["@id"]
-                    annotation_id = "{activity}/annotations/aqs/{id}".format(
-                        activity=activity.id, id=model_id
-                    )
-                    annotations.append(
-                        Annotation(id=annotation_id, source="AQS plugin", body=nb2annotation)
-                    )
-    # TODO fix the path accordingly
-    if os.path.exists(graphvis.graph_vis_annotation_path):
-        for p in graphvis.graph_vis_annotation_path.iterdir():
-            if p.match("*json"):
-                print(f"found json annotation: {p}")
-                print(open(p).read())
-
-            elif p.match("*jsonld"):
-                aqs_annotation = graphvis.load_model(p)
-                print(f"found jsonLD annotation: {p}\n", json.dumps(aqs_annotation, sort_keys=True, indent=4))
-
-                # this will make annotations according to https://odahub.io/ontology/
-                model_id = aqs_annotation["@id"]
-                annotation_id = "{activity}/annotations/aqs/{id}".format(
-                    activity=activity.id, id=model_id
-                )
-                p.unlink()
-                annotations.append(
-                    Annotation(id=annotation_id, source="AQS plugin", body=aqs_annotation)
-                )
-    else:
-        print("nothing to process in process_run_annotations")
-
-    return annotations
-
-
-@hookimpl
-def pre_run(tool):
-    print(f"\033[31mhere we will prepare hooks for astroquery, tool given is {tool}\033[0m")
-
-    sitecustomize_dir = pathlib.Path(project_context.metadata_path, ENTITY_METADATA_GRAPHVIS_DIR)
-
-    if not sitecustomize_dir.exists():
-        sitecustomize_dir.mkdir(parents=True)
-
-    os.environ["PYTHONPATH"] = f"{sitecustomize_dir}:" + os.environ.get('PYTHONPATH', "")
-
-    sitecustomize_path = os.path.join(sitecustomize_dir, "sitecustomize.py")
-
-    print(f"\033[34msitecustomize.py as {sitecustomize_path}\033[0m")
-    # TODO this part configurable?
-    open(sitecustomize_path, "w").write("""
-print(f"\033[31menabling hooks for astroquery\033[0m")  
-
-import aqsconverters.aq
-
-aqsconverters.aq.autolog()
-""")
 
 
 def _run_id(activity_id):
@@ -172,26 +59,6 @@ def graphvis():
 def show_graph_image(revision="HEAD", paths=os.getcwd(), filename="graph.png", no_oda_info=True, input_notebook=None):
     filename = graph_utils.build_graph_image(revision, paths, filename, no_oda_info, input_notebook)
     return Image(filename=filename)
-
-
-@graphvis.command()
-@click.option(
-    "--revision",
-    default="HEAD",
-    help="The git revision to generate the log for, default: HEAD",
-)
-@click.option("--input-notebook", default=None, help="Input notebook to process")
-@click.argument("paths", type=click.Path(exists=False), nargs=-1)
-def inspect(revision, paths, input_notebook):
-    """Inspect the input entities within the graph"""
-
-    path = paths
-    if paths is not None and isinstance(paths, click.Path):
-        path = str(path)
-
-    graph_utils.inspect_entity_inputs(revision, path, input_notebook)
-
-    return ""
 
 
 @graphvis.command()
