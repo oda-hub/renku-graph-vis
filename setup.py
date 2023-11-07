d05eb5863a22fcbc68e203e54201de62bd777b6d
burnout87 <gabriele.barni@gmail.com>
2023-11-07 18:22:28 +0100

    trying freezing nb2workflow

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
     'rdflib',
     'bs4',
     'renku==2.7.0',
-    'nb2workflow<=1.3.42,>=1.3.41',
+    'nb2workflow>=1.3.41,',
     'pyvis==0.3.0',
     'pydotplus',
     'lockfile'
